c0d332634b07e77c52bb5c04360a322c8aaa511f
maddie-y-k <136375975+maddie-y-k@users.noreply.github.com>
2023-08-18 14:52:20 -0400

    Initial Site Version
    First version of portfolio site.

---
+++ nav_links.js
@@ -0,0 +1,11 @@
+document.write("<header>");
+document.write("    <h2><a href=\"#\" class=\"logo\">LOGO<\/a><\/h2>");
+document.write("    <ul class=\"nav_links\">");
+document.write("        <li class=\"link\"><a href=\"index.html\">HOME<\/a><\/li>");
+document.write("        <li class=\"link\"><a href=\"about.html\">ABOUT<\/a><\/li>");
+document.write("        <li class=\"link\"><a href=\"resume.html\">RESUME<\/a><\/li>");
+document.write("        <li class=\"link\"><a href=\"projects.html\">PROJECTS<\/a><\/li>");
+document.write("        <li class=\"link\"><a href=\"contact.html\">CONTACT<\/a><\/li>");
+document.write("    <\/ul>");
+document.write("<\/header>");
+
